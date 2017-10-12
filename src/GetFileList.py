da07066dfd1f3d902b7e8537e15cccacfc28cec3
YukiUeda <ikuyadeu@yahoo.co.jp>
2017-10-12 14:19:37 +0900

    Fix output and readability

--- src/GetFileList.py
+++ src/GetFileList.py
@@ -13,8 +13,8 @@ Output:
 import sys
 import csv
 from urllib.parse import quote_plus
-import MySQLdb
 from collections import defaultdict
+import MySQLdb
 
 def main():
     """
@@ -33,8 +33,8 @@ def main():
         passwd = ""
 
     # Define dictionary
-    t_revisionDic = defaultdict(lambda: [])
-    t_fileDic = defaultdict(lambda: [])
+    t_revision_dic = defaultdict(lambda: [])
+    t_file_dic = defaultdict(lambda: [])
 
     # Connect DB
     connection = MySQLdb.connect(db=current_db, user=user, passwd=passwd)
@@ -42,7 +42,7 @@ def main():
 
     # Get changes
     sys.stdout.write("\rCollecting changes...")
-    sql = "SELECT id, ch_Id, ch_changeId \
+    sql = "SELECT id, ch_Id, ch_changeId, ch_changeIdNum \
     FROM t_change"
     cursor.execute(sql)
     changes = cursor.fetchall()
@@ -66,9 +66,9 @@ def main():
 
     # Store data into t_revisionDic
     for revision in revisions:
-        t_revisionDic[revision[2]].append(revision)
-    for f in files:
-        t_fileDic[int(f[1])].append(f)
+        t_revision_dic[revision[2]].append(revision)
+    for rev_file in files:
+        t_file_dic[int(rev_file[1])].append(rev_file)
 
     # File list for output
     output_files = []
@@ -76,16 +76,18 @@ def main():
     # Search from changes
     changes_len = len(changes)
     for i, change in enumerate(changes):
-        ch_revisions = t_revisionDic[change[0]]
+        ch_revisions = t_revision_dic[change[0]]
         ch_id = change[1]
         ch_change_id = change[2]
+        ch_change_id_num = change[3]
         revisions_len = len(ch_revisions)
         # Search from revisions
         for j, revision in enumerate(ch_revisions):
-            rev_files = t_fileDic[revision[0]]
+            rev_files = t_file_dic[revision[0]]
             rev_id = revision[1]
             rev_change_id = revision[2]
-            output_files += [[ch_id, rev_id, quote_plus(rev_file[0]), ch_change_id]
+            output_files += [[ch_id, ch_change_id, ch_change_id_num,
+                              rev_id, rev_change_id, quote_plus(rev_file[0])]
                              for rev_file in rev_files]
             sys.stdout.write("\rChange: %d / %d, Revision: %d / %d" %
                              (i, changes_len, j, revisions_len))
@@ -93,7 +95,8 @@ def main():
     # Output
     with open(current_db + ".csv", 'w') as csvfile:
         writer = csv.writer(csvfile, lineterminator='\n')
-        writer.writerow(["ch_id", "rev_id", "f_file_name"])
+        writer.writerow(["ch_id", "ch_change_id", "ch_change_id_num",
+                         "rev_id", "rev_change_id", "f_file_name"])
         writer.writerows(output_files)
 
 if __name__ == '__main__':
